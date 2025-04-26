30a5559fcc3045bff8364a3ff4a221b879165c0b
el programo W id <androidwalid7@gmail.com>
2025-04-26 16:27:52 +0100

    added contact mail in footer too, as well as work experiences links to projects section and filled some videos demo

--- portfolio/src/app/projects/projects.component.ts
+++ portfolio/src/app/projects/projects.component.ts
@@ -17,48 +17,48 @@ export class ProjectsComponent {
       title: 'Projet Thales – Cloud, DevOps & Sécurité',
       description: 'Stage alterné chez Thales : Cloud, DevOps, sécurité et automatisation.',
       image: '../assets/thales.png',
-      video: '../assets/thales.png',
-      members: ['Toi'],
+      video: '../assets/notyet.mp4',
+      members: ['moi'],
       tools: ['Terraform', 'GitLab CI/CD', 'Docker', 'Ansible']
     },
     {
       title: 'IA – Détection de maladies (Diabète, Cœur)',
       description: 'Modèle IA pour la prédiction de maladies chroniques.',
       image: '../assets/diaheart.png',
-      video: '../assets/thales.png',
-      members: ['Toi'],
+      video: '../assets/appML.mp4',
+      members: ['moi'],
       tools: ['Python', 'Scikit-learn', 'Pandas', 'Jupyter']
     },
     {
       title: 'E-blog intelligent avec recommandation IA',
       description: 'Blog avec moteur de recommandation d’articles personnalisé.',
       image: '../assets/eblog.png',
-      video: '../assets/thales.png',
-      members: ['Toi'],
+      video: '../assets/notyet.mp4',
+      members: ['moi'],
       tools: ['Angular', 'Python', 'Flask', 'Machine Learning']
     },
     {
       title: 'Plateforme étudiants-lauréats avec IA de filtrage de CV',
       description: 'Application de mise en relation avec UI/UX, IA et frontend Angular.',
       image: '../assets/laureat.png',
-      video: '../assets/thales.png',
-      members: ['Toi', 'Camarade 1', 'Camarade 2'],
+      video: '../assets/notyet.mp4',
+      members: ['moi', '3 membres'],
       tools: ['Angular', 'Spring Boot', 'Python', 'Figma']
     },
     {
       title: 'Assistant vocal – Stage OCP 2024',
       description: 'Assistant vocal pour automatiser les tâches répétitives.',
       image: '../assets/assistant.png',
-      video: '../assets/thales.png',
-      members: ['Toi'],
+      video: '../assets/notyet.mp4',
+      members: ['moi'],
       tools: ['Python', 'SpeechRecognition', 'Flask']
     },
     {
       title: 'Application de réservation de tickets en C',
       description: 'Application en C développée en équipe pour la gestion de tickets.',
       image: '../assets/cc.png',
-      video: '../assets/thales.png',
-      members: ['Toi', '3 membres'],
+      video: '../assets/notyet.mp4',
+      members: ['moi', '3 membres'],
       tools: ['C', 'UML']
     }
   ];
